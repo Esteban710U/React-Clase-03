b18416414d83b0368ed20d4142d7ea504e17e48c
Esteban <2008esteban12@gmail.com>
2026-08-06 09:01:16 -0500

    clase

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/React-clase-03'
-})
+  base: "/React-Clase-03/",
+});
